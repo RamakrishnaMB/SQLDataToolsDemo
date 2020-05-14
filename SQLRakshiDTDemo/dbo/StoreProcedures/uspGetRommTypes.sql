2be91eee49bbb8722281c06a91ff4207e3eac6c3
findingramakrishnamb <findingramakrishnamb@gmail.com>
2020-05-14 23:35:26 +0530

    new sp

--- SQLRakshiDTDemo/dbo/StoreProcedures/uspGetRommTypes.sql
+++ SQLRakshiDTDemo/dbo/StoreProcedures/uspGetRommTypes.sql
@@ -1,8 +1,9 @@
-﻿CREATE PROCEDURE [dbo].[uspGetRommTypes]
+﻿CREATE PROCEDURE [dbo].[uspGetRoomTypes]
 	@RoomTypeID int
 	
 AS
 	SELECT RoomType AS RoomType, CASE WHEN IsAC =1 THEN 'AC Room' ELSE 'NON AC' END
 	FROM RoomType
 	WHERE RoomTypeId = @RoomTypeID
+	SELECT 'Room Type'
 RETURN 0
