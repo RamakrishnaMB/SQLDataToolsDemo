fbc397058fbe0838f6f3709a9c5bc9f47fe111ba
findingramakrishnamb <findingramakrishnamb@gmail.com>
2020-05-14 23:32:38 +0530

    StoredProcedure is added

---
+++ SQLRakshiDTDemo/dbo/StoreProcedures/uspGetRommTypes.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[uspGetRommTypes]
+	@RoomTypeID int
+	
+AS
+	SELECT RoomType AS RoomType, CASE WHEN IsAC =1 THEN 'AC Room' ELSE 'NON AC' END
+	FROM RoomType
+	WHERE RoomTypeId = @RoomTypeID
+RETURN 0
