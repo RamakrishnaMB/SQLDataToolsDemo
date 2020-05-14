86f1dc37547c11b6cf772a8f576b59fb39636cbe
findingramakrishnamb <findingramakrishnamb@gmail.com>
2020-05-14 23:22:23 +0530

    Created view

---
+++ SQLRakshiDTDemo/dbo/View/BookingDetails.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW [dbo].[BookingDetails]
+	AS SELECT B.BookingID,C.Name,C.Telephone,c.Email,C.ProfilePic,
+	R.RoomType,B.CheckInDate,B.CheckOutDate FROM Booking B
+	INNER JOIN Customer C ON C.CId = B.CId
+	LEFT OUTER JOIN RoomType R ON R.RoomTypeId = B.RoomId
